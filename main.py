08091ae4aa87a79bed7b74ae369c4a55af2d932d
Zain Raza <zrabbas110@gmail.com>
2024-05-30 23:01:59 +0500

    assigment

---
+++ main.py
@@ -0,0 +1,79 @@
+# problem 1
+# Write a program to check whether a person is eligible to vote or not?
+# solution
+# age=int(input("Enter your age \t"))
+# if age>=18:
+#     print("you are eligible to vote")
+# else:
+#     print("you are not eligible  to vote")
+
+# =======================================================
+# problem 2
+# Write a program to check char is vowel or not.
+
+# soluton
+# char=input("Enter  char \t")
+# if char=="a" or char=="e" or  char=="i" or char=="o" or char=="u"or char=="A" or char=="I" or  char=="O" or char=="U" or char=="E"    :
+#     print("Vowel")  
+# else:
+#     print("not Vowel")
+# ======================================================
+# problem 3
+# Write a program to check the number is positive or negative. User input.
+
+# solution
+# num=int(input("Enter number\t"))
+# if num>=0:
+#     print("positive ")
+# else:
+#     print("negative")
+
+
+# ==============================================
+# proble 4
+# Write a program to check whether a number is odd or even?
+# solution
+# num=int(input("Enter number \n"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+# ===============================================
+# problem 5
+# Write a program to display the grade of the user in subject A, ask user marks obtained out of 100
+# solution 
+# marks=int(input("Enter subject marks out of 100 \n"))
+# if marks>=90:
+#     print("A+")
+# elif marks>=80:
+#     print("A")
+# elif marks>=70:
+#     print("B+")
+# elif marks>=60:
+#     print("b")
+# elif marks>=50:
+#     print("c")
+# else:
+#     print("fails")
+# ==================================================
+# problem 6
+# Write a program to check whether a number is divisible by 7
+# solution
+# num=int(input(("Enter number ")))
+# if num%7==0:
+#     print(num,"number is divide by 7")
+# else:
+#         print(num," number is not divide by 7")
+# =======================================================
+# problem 7
+# Write a program to check if year is leap year.
+# solution 
+# leap_year= int(input("Enter your years \n"))
+# if leap_year%4==0:
+#     print("yes this is leap years")
+# else:
+#     print("no this is not leap year")
+
+    
+    
